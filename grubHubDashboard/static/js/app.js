2af8797858038966a1f37198e5942eabde1bbd86
tanmayrp <tanmayrp@gmail.com>
2021-07-24 17:40:33 -0400

    Removing unnecessary references and files. Added Earnings by Restaurants plot (initial).

--- grubHubDashboard/static/js/app.js
+++ grubHubDashboard/static/js/app.js
@@ -1,8 +1,8 @@
-// function unpack(rows, index) {
-//   return rows.map(function(row) {
-//     return row[index];
-//   });
-// }
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
 
 function buildDashboards() {
 
@@ -11,10 +11,29 @@ function buildDashboards() {
   d3.json(url).then(function(response) {
 
     console.log(response);
+    var establishments = response.map(d =>  d.establishment);
+    console.log(establishments);
 
-    // var highPrices = unpack(response.establishment, 2);
-    // console.log(highPrices);
-    
+    var earnings = response.map(d => d.total);
+    console.log(earnings);
+
+    var trace = {
+      x: establishments,
+      y: earnings,
+      type: "bar"
+    };
+    // Create the data array for our plot
+    var data = [trace];
+
+    // Define the plot layout
+    var layout = {
+      xaxis: { title: "Establishments" },
+      yaxis: { title: "Total Earned"}
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("establishmentsEarningsBar", data, layout);
+
   });
 }
 
