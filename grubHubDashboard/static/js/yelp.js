90e13c15df7cb851d665ef8a575b8dcb4d1192ea
tanmayrp <tanmayrp@gmail.com>
2021-07-26 11:13:54 -0400

    ,

--- grubHubDashboard/static/js/yelp.js
+++ grubHubDashboard/static/js/yelp.js
@@ -3,11 +3,46 @@ function buildYelpEarningsRatingsChart() {
   const url = "/api/yelpearningsratings";
   d3.json(url).then(function(response) {
     console.log(response);
+
+    var earningsList = [];
+    var ratingsList = [];
+    // console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      ratingsList.push(response[i]["rating"]);
+      earningsList.push(response[i]["total"]);
+    }
+
     // var totalMilesDiv = document.getElementById('totalMilesDiv');
     // totalMilesDiv.innerHTML += response.totalMiles;
 
     // var totalMileagePayDiv = document.getElementById('totalMileagePayDiv');
     // totalMileagePayDiv.innerHTML += response.totalMileagePay;
+
+    //scatter plot for Ratings vs. Earnings
+
+    var trace1 = {
+      x: ratingsList,
+      y: earningsList,
+      mode: 'markers',
+      type: 'scatter',
+      name: 'Team A',
+      text: earningsList,
+      marker: { size: 12 }
+    };
+    
+    var data = [ trace1 ];
+    
+    var layout = {
+      xaxis: {
+        range: [ 0, 5.5 ]
+      },
+      yaxis: {
+        range: [0, 600]
+      }
+      // title:'Data Labels Hover'
+    };
+    
+    Plotly.newPlot('EarningsRatingsDiv', data, layout, {responsive: true});
 
   });
 
